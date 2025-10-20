f777326afb2772369dd3f99dbc79a2270e82f7e9
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2025-10-20 15:35:01 -0400

    Setup agentic coding

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule ReviewRoom.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:tidewave, "~> 0.1", only: [:dev]}
     ]
   end
 
